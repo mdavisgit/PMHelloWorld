7372a2b1c5a158031a21124d396f233e1c8c3714
Michael Davis <michael@daviskingdom.com>
2015-03-19 21:47:49 +0100

    Initial Commit

---
+++ helloWorld/src/helloWorld/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace helloWorld;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+	
+	public function onEnable() {
+		$message = "Hello World\n";
+		$message2 = "First Message";
+		$this->getLogger()->info($message . $message2);
+	}
+	
+	
+	public function on
+	
+	
+	
+	public function onDisable() {
+		
+	}
+}
